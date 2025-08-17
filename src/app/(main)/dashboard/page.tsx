325722d1a973f041eaf9478264a3535145af292b
Ali Khosrojerdi <ali.khosro72@gmail.com>
2025-08-17 19:52:30 +0330

    minor change

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import style from './dashboad.module.scss'
+
+function page() {
+    return (
+        <div className={style.content}>
+            <h2>Welcome to the Dashboard</h2>
+        </div>
+    )
+}
+
+export default page
